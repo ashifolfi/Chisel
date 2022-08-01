a5c984ed94f28e43ae282cb16998d98c5160bc09
Ashi <anticream39@yahoo.com>
2022-08-01 12:04:28 -0400

    dockable windows and root panel functional

--- scripts/UI/RootPanel.cs
+++ scripts/UI/RootPanel.cs
@@ -1,10 +1,13 @@
 ﻿using Godot;
 using System;
+using System.Collections.Generic;
 
 namespace Chisel.UI
 {
     public class RootPanel : Panel
     {
+        public List<WindowDockContainer> DockContainers = new List<WindowDockContainer>();
+
         // On ready resize the container to fit the current window size
         public override void _Ready()
         {
--- scripts/UI/WindowDockContainer.cs
+++ scripts/UI/WindowDockContainer.cs
@@ -2,8 +2,26 @@
 
 namespace Chisel.UI
 {
-    public class WindowDockContainer : Container
+    public class WindowDockContainer : PanelContainer
     {
+        // Add self (we use this for all of our custom nodes)
+        private WindowDockContainer Self;
+        private RootPanel Root;
+        public override void _Ready()
+        {
+            // Make self contain us
+            Self = GetNode<WindowDockContainer>(".");
+            Root = GetNode<RootPanel>("/root/UI");
+            Root.DockContainers.Add(Self);
+            
+            // Always clip content to prevent weird oddities
+            RectClipContent = true;
+        }
         
+        public void DockContainer(DockableDialog Dialog)
+        {
+            // Tie this container to the dialog and mark it as docked
+            Dialog.ToDock(Self);
+        }
     }
 }
