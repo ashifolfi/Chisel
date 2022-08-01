4b33e83c7b94242a277e181d4369232a7ac81e53
Ashi <anticream39@yahoo.com>
2022-08-01 12:35:29 -0400

    Reorganize UI folder, Convert Tools to a dock

---
+++ scripts/UI/Docks/ToolsDock.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+namespace Chisel.UI
+{
+    public class ToolsDock : DockableDialog
+    {
+        private MeshManager MeshManager;
+
+        public override void _Ready()
+        {
+            base._Ready();
+            MeshManager = GetNode<MeshManager>("/root/UI/3dEnv/MeshManager");
+        }
+        
+        private void _on_CreateCube_pressed()
+        {
+            GetNode<WindowDialog>("/root/UI/CubeCreate").Popup_();
+        }
+
+        private void _on_Select_pressed()
+        {
+            MeshManager.SelectedMesh = null;
+        }
+	
+        private void _on_Delete_pressed()
+        {
+            if (MeshManager.SelectedMesh != null)
+            {
+                MeshManager.DeleteMesh(MeshManager.SelectedMesh);
+            }
+        }
+    }
+}
