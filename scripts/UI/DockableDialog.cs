2a2a1fb5ca1756a5cca1aeb4c77b32b3d8c43861
Ashi <anticream39@yahoo.com>
2022-08-01 10:44:21 -0400

    new controls, fix bugs, 3d grid brush drawing

---
+++ scripts/UI/DockableDialog.cs
@@ -0,0 +1,9 @@
+﻿using Godot;
+
+namespace Chisel.UI
+{
+    public class DockableDialog : Node
+    {
+        
+    }
+}
---
+++ scripts/UI/RootPanel.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+using System;
+
+namespace Chisel.UI
+{
+    public class RootPanel : Panel
+    {
+        public override void _Draw()
+        {
+            // Resize root panel to the window size
+            if (RectSize != OS.WindowSize)
+            {
+                RectSize = OS.WindowSize;
+            }
+        }
+    }
+}
